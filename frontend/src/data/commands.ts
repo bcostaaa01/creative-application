9e15c35cac5f6f142a8741af436f233be72b8249
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-11-02 19:46:26 +0100

    feat: use name of the company to make it even more awesome

--- frontend/src/data/commands.ts
+++ frontend/src/data/commands.ts
@@ -1,7 +1,7 @@
 export const commands = [
   "$ whoami",
   "Hi, I am Bruno Costa, a passionate web developer excited about your projects.",
-  "I want to apply to your company. Why?",
+  "I want to apply to MASSIVE ART. Why?",
   "Your focus on creative web applications is exactly what inspires me every day!",
   "Why should you hire me?",
   "I am very passionate, positive, creative, a team player, and always seeking new opportunities to learn new things and think of different ways to solve the same problem.",
